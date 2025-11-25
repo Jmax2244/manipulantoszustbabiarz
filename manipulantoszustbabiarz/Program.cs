75595b0cd3d44cea7488c6c61a56a2a81ec078ed
3988200 <3988200@TE-GDA-D-D28J15>
2025-11-25 11:27:26 +0100

    Dodaj pliki projektów.

---
+++ manipulantoszustbabiarz/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Podaj zdanko:");
+        string tekst = Console.ReadLine();
+
+        Console.WriteLine($"Ile mierzysz ile wazysz: {tekst.Length}");
+
+        Console.WriteLine("Wielkie litery:");
+        Console.WriteLine(tekst.ToUpper());
+
+        Console.WriteLine("Małe literki sigma");
+        Console.WriteLine(tekst.ToLower());
+    }
+}

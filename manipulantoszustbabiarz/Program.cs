7ffd750b5ef19da40b0e9f36110292ce291cb791
Tobi <Oszust@gmail.com>
2025-11-25 11:56:41 +0100

    zad 1.2

--- manipulantoszustbabiarz/Program.cs
+++ manipulantoszustbabiarz/Program.cs
@@ -9,8 +9,8 @@ class Program
 
         Console.WriteLine($"Ile wazysz ile mierzysz: {tekst.Length}");
 
-        Console.WriteLine("Wielki czy maly hmm?");
-        Console.WriteLine("Wpisz 'wielki' albo 'maly':");
+        Console.WriteLine("Wielki czy maly hmm? a moze pierwszak? haha");
+        Console.WriteLine("Wpisz 'wielki', 'maly' albo 'pierwszak':");
         string wybor = Console.ReadLine().ToLower().Trim();
 
         if (wybor == "wielki")
@@ -23,9 +23,22 @@ class Program
             Console.WriteLine("Tekst malymi literami:");
             Console.WriteLine(tekst.ToLower());
         }
+        else if (wybor == "pierwszak")
+        {
+            if (!string.IsNullOrEmpty(tekst))
+            {
+                string wynik = char.ToUpper(tekst[0]) + tekst.Substring(1).ToLower();
+                Console.WriteLine("wielki pierwszak:");
+                Console.WriteLine(wynik);
+            }
+            else
+            {
+                Console.WriteLine("Eee no nic nie wpisales koleszko...");
+            }
+        }
         else
         {
-            Console.WriteLine("Zly wybur koleszko Wpisz 'wielki' lub 'maly'.");
+            Console.WriteLine("Zly wybur koleszko. Wpisz 'wielki', 'maly' albo 'pierwszak'.");
         }
     }
 }
